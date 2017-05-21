2268186f7776504d85cf048bbde31257799fc4fc
Daniel Halliday <daniel.j.halliday@gmail.com>
2017-05-21 22:05:35 +0100

    Added board to do manual check missing words

--- boggle.py
+++ boggle.py
@@ -261,7 +261,15 @@ def nextLetterArrayBuilder(r, c, board, boardSize, visitedSquares, candidateStri
     return solutions
 
 # print("Test all solutions generated using r:0, c:0 = ", nextLetterArrayBuilder(0, 0, board, boardSize, visitedSquares, candidateString) )
-print(board)
+print("---------------------")
+print(board[0][0], " ! ", board[0][1], " ! ", board[0][2], " ! ", board[0][3])
+print("---------------------")
+print(board[1][0], " ! ", board[1][1], " ! ", board[1][2], " ! ", board[1][3])
+print("---------------------")
+print(board[2][0], " ! ", board[2][1], " ! ", board[2][2], " ! ", board[2][3])
+print("---------------------")
+print(board[3][0], " ! ", board[3][1], " ! ", board[3][2], " ! ", board[3][3])
+print("---------------------")
 
 # Build 3rd square solution to add third valid character to the solutionArray without
 # doubling back on a previously seen square
